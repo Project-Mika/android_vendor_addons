8233a0bbda9bb1bef8d94dcd78edd8fed2580680
zclkkk <zclkkk@aliyun.com>
2022-10-22 20:08:25 +0800

    addons: Import Nothing audio
    * From Nothing/SpacewarEEA/Spacewar:12/SKQ1.211230.001/1663735507:user/release-keys

--- config.mk
+++ config.mk
@@ -4,3 +4,12 @@ PRODUCT_PACKAGES += \
     PrcCalculator \
     PrcGallery2 \
     Via
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/addons/audio,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=01_pneumatic.ogg \
+    ro.config.notification_sound=01_oi!.ogg \
+    ro.config.alarm_alert=01_bedside.ogg
